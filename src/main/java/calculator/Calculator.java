034dfd6249b3bf471cecd025c3892970f08a6198
wmw04087 <wmw04087@gmail.com>
2025-10-16 01:10:03 +0900

    feat: 빈 문자열 또는 공백 입력 시 기본값 0 반환 구현

---
+++ src/main/java/calculator/Calculator.java
@@ -0,0 +1,22 @@
+package calculator;
+
+import calculator.domain.CustomDelimiter;
+import calculator.domain.Numbers;
+import calculator.view.InputView;
+import calculator.view.OutputView;
+import java.util.Optional;
+
+public class Calculator {
+    private final InputView inputView = new InputView();
+    private final OutputView outputView = new OutputView();
+
+    public void run() {
+        outputView.askInputString();
+        String input = inputView.inputString();
+
+        if (input.isBlank()) {
+            outputView.showResult(0);
+            return;
+        }
+    }
+}
---
+++ src/main/java/calculator/view/OutputView.java
@@ -0,0 +1,12 @@
+package calculator.view;
+
+public class OutputView {
+
+    public void askInputString() {
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+    }
+
+    public void showResult(int number) {
+        System.out.println("결과 : " + number);
+    }
+}
